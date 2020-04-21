04a403a6121879185b16f9e54002ab673af81d8d
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-04-21 11:17:39 -0700

    use extension

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mimeTypes = require('./file-extension-to-mime-types.json');
 
 function lookup(ext) {
-  return mimeTypes['.' + ext];
+  return mimeTypes[ext];
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const lib = require('./');
 
-assert(lib.lookup);
-assert('py', lib.lookup('py'));
+assert('function' === typeof lib.lookup);
+assert('text/x-python' === lib.lookup('.py'));
 assert(undefined === lib.lookup());
